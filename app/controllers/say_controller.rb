fb3bac51688841246d3e79273cec6f0c8cea92b7
Trygve <trygvejohannessen88@gmail.com>
2022-02-13 23:13:14 +0100

    Revert "lord"
    This reverts commit aec1e864201ab9cd01ea900d22e53a17445a7352.

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -3,7 +3,7 @@ class SayController < ApplicationController
     Tmdb::Api.key("b86ed2181541cb582c6118debab2480b")
     @search = Tmdb::Search.new
     @search.resource('movie') # determines type of resource
-    @search.query('Lord') # the query to search against
+    @search.query('Matrix') # the query to search against
     result_full = @search.fetch # makes request
 
     @result = []
